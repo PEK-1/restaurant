7bd40bae5a1e343d08bbee2f6b2873b18ed3ee0b
Prathyusha Gadi <gadiprathyusha2712@gmail.com>
2024-10-25 15:04:13 -0500

    gallery.jsx

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import { BsInstagram, BsArrowLeftShort, BsArrowRightShort } from 'react-icons/bs';
 
+import { SubHeading } from '../../components';
+import { images, data } from '../../constants';
 import './Gallery.css';
 
-const Gallery = () => (
-  <div>
-    Gallery
-  </div>
-);
+const Gallery = () => {
+  
+  
+  return (
+    <div className='app__gallery'>
+      <div className='app__gallery-content'>
+        <SubHeading title='Instagram' />
+        <h1 className='headtext__cormorant'>Photo Gallery</h1>
+        <p className='p__opensana'style={{color: '#AAA', marginTop: '2rem'}}></p>
+      </div>
+    </div>
+  );
+}
 
 export default Gallery;
